eded9360b9d2b0f888216c916211f53d4ff9622f
Rafael Lima <rafa.cbl@gmail.com>
2023-06-29 13:59:15 -0300

    add run_all function to run the entire pipeline

--- lua/nvim-rspec/init.lua
+++ lua/nvim-rspec/init.lua
@@ -10,12 +10,16 @@ local function run_nearest_spec()
   local line = vim.fn.line(".")
 
   local cmd = ':!bundle exec rspec ' .. spec_file .. ':' .. line .. ''
-  -- vim.cmd("split | terminal")
-  -- local cmd = ':call jobsend(b:terminal_job_id, "bundle exec rspec ' .. spec_file .. ':' .. line .. '\\n")'
+  vim.cmd(cmd)
+end
+
+local function run_all()
+  local cmd = ':!bundle exec rspec spec/'
   vim.cmd(cmd)
 end
 
 return {
   run_spec = run_spec,
   run_nearest_spec = run_nearest_spec,
+  run_all = run_all,
 }
